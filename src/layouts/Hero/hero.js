a31fb7cba72ee5ac501412297023981e8b514e9a
Warren Chan <warrenchan@warren-2.local>
2017-07-25 01:32:59 -0600

    Created Hero component

---
+++ src/layouts/Hero/hero.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styles from './styles.css';
+
+const Hero = () => (
+  <div className="heroImageContainer">
+    <div className="heroTitleContain" >
+      <p className="heroTitle">Tango</p>
+      <p className="heroSubTitle" >Tropical grill <br/> Vietnamese & Thai cuisine </p>
+      <a className="heroButton" href="">Contact</a>
+    </div>
+  </div>
+)
+
+export default Hero;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,6 +4,7 @@ import Link from "gatsby-link"
 import Helmet from "react-helmet"
 
 import NavBar from './NavBar'
+import Hero from './Hero'
 
 import "../css/typography.css"
 import styles from "../css/styles.css"
@@ -26,11 +27,7 @@ export default class Template extends React.Component {
           ]}
         />
         <NavBar />
-        <div className="heroImageContainer">
-          <div>
-            <h2 className="heroTitle">Tango</h2>
-          </div>
-        </div>
+        <Hero />
       </div>
     )
   }
